519672958d51d6ada5eb05d963a10590c92c240f
izzy <izder456@disroot.org>
2023-04-07 18:59:18 -0500

    fix console output line

--- paintRoomCalc_human.cr
+++ paintRoomCalc_human.cr
@@ -69,12 +69,10 @@ end
 width = prompt_user("Enter the width of the room (in feet):").to_f
 depth = prompt_user("Enter the depth of the room (in feet):").to_f
 height = prompt_user("Enter the height of the room (in feet):").to_f
-layer_thickness = prompt_user("Enter the thickness of each layer (in mils):").to_f / 12000.0
+layer_thickness = prompt_user("Enter the thickness of each layer (in mils):").to_f / 12.0 / 1000.0
 
 # Calculate the number of layers needed to fill the room
 num_layers = calculate_num_layers(width, depth, height, layer_thickness)
 
-layer_thickness_R = (layer_thickness * 1000.0).format(decimal_places: 2)
-
 # Print the result to the console
-puts "To completeliy fill a room with dimensions #{width} x #{depth} x #{height} feet using ~#{layer_thickness_R}-inch-thick layers of paint, you would need #{num_layers} layers."
+puts "To completeliy fill a room with dimensions #{width} x #{depth} x #{height} feet using ~#{layer_thickness}-inch-thick layers of paint, you would need #{num_layers} layers."
